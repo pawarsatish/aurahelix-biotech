d68e07a99dba0c7f73924029598afb61e2a27113
Satish Pawar <pawarsatish132@gmail.com>
2026-08-14 15:16:14 +0530

    fix: core tailwind packages path

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import './style.css'
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx' // Make sure the path points to your new App.tsx
+/// <reference types="vite/client" />
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx'; // Make sure the path points to your new App.tsx
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('app')!).render(
   <React.StrictMode>
